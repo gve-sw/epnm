6f9ff9e15bd74c98be1b06972659bbab2225fdf4
redelane <redelane@cisco.com>
2017-04-05 14:33:37 -0400

    Added functionality to epnm class. Can now find epnm id from device mgmt ip address and the software type. Still need to test

--- step 2/epnm.py
+++ step 2/epnm.py
@@ -13,9 +13,49 @@ Class EPNM(object):
 		self.verify = verify
 		self.url = 'https://' + self.ip + '/webacs/api/v1/'
 
-	def getDevices(self):
-		getUrl = self.url + 'data/Devices.json'
-		response = requests.get(getUrl, auth=(self.user, self.password), verify=self.verify)
+	def getDevice(self, dev_id=''):
+		getURL = self.url + 'data/Devices'
+
+		if dev_id != '':
+			getURL = getURL + dev_id + '.json'
+		else:
+			getURL += '.json'
+
+		response = requests.get(getURL, auth=(self.user, self.password), verify=self.verify)
 		return response
 
-	
+	def getIDfromIP(self, dev_mgmt_ip):
+
+		dev_list_json = self.getDevices()
+		dev_id_list = self.getDevIDs(dev_list_json)
+
+		for i in dev_id_list:
+
+			dev_info_json = self.getDevice(i)
+			
+			resp_list = dev_info_json.json()['queryResponse']['entity']
+			dev_info_dict = resp_list[0]['devicesDTO']
+			
+			if dev_info_dict['ipAddress'] == dev_mgmt_ip:
+				return dev_info_dict['deviceId']
+		
+		return "No such device found"
+
+
+	def getDevIDs(self, devices_resp):
+
+		new_resp = devices_resp.json()['queryResponse']['entityId']
+
+		IDs = []
+		for entity in new_resp:
+			IDs.append(entity['$'])
+
+		return IDs
+
+	def getSWfromID(self, dev_id):
+
+		dev_info_json = self.getDevice(dev_id)
+		resp_list = dev_info_json.json()['queryResponse']['entity']
+		dev_info_dict = resp_list[0]['devicesDTO']
+
+		return dev_info_dict['softwareType']
--- step 2/import_info.py
+++ step 2/import_info.py
@@ -1,12 +1,13 @@
 import csv
 import pandas as pd
-import ./test_scripts/get_device_info.py
+from epnm.py import EPNM
 from device.py import Device, ASR, NCS
 
 host = "198.18.134.7"
 username = "user"
 password = "Tester123"
 
+manager = EPNM(host, username, password)
 
 dev_addr_df = pd.read_csv('dev_address.csv')
 connections_df = pd.read_csv('connections.csv')
@@ -22,8 +23,8 @@ for i in range(0, num_rows):
 	device_name = dev_addr_df.loc[i]['Device Name']
 	device_mgmt_ip = dev_addr_df.loc[i]['Mgmt']
 	device_lo_ip = dev_addr_df.loc[i]['Loopback0']
-	device_epnm_id = getID(device_mgmt_ip)
-	soft_type = getSW(device_epnm_id)
+	device_epnm_id = manager.getIDfromIP(device_mgmt_ip)
+	soft_type = getSWfromID(device_epnm_id)
 
 	if soft_type == 'IOS-XE':
 		devices[device_name] = NCS(device_name, device_mgmt_ip, device_lo_ip, device_epnm_id)
